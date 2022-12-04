44f060da36c66c1c18ac7a5347dc01c0068a4ea0
Tom Cowell <tom.blacknight@gmail.com>
2022-12-04 19:03:39 +0000

    feat: day 4 part 2

--- Day04/CampCleanup.cs
+++ Day04/CampCleanup.cs
@@ -2,44 +2,43 @@ namespace AdventOfCode2022.Day04;
 
 public class CampCleanup : ChallengeBase
 {
-    public CampCleanup(string[] data) : base(data)
-    {
-    }
+    public CampCleanup(string[] data) : base(data) {}
 
     protected override int Part1() => ChallengeDataRows
-            .Select(ThePairAssignments)
-            .Count(OverlappingPairs);
+        .Select(PairAssigmmentsToRanges)
+        .Count(PairsThatContainOtherPair);
 
-    private static bool OverlappingPairs((Range pair1, Range pair2) pairs) => 
+    private static bool PairsThatContainOtherPair((Range pair1, Range pair2) pairs) => 
         pairs.pair1.Contains(pairs.pair2) 
         || pairs.pair2.Contains(pairs.pair1);
 
-    private static bool RangeContains(Range range0, Range range1)
+    private static (Range pair1, Range pair2) PairAssigmmentsToRanges(string row)
     {
-        return range0.Start.Value >= range1.Start.Value
-            && range0.End.Value <= range1.End.Value;
-    }
-
-    private static (Range pair1, Range pair2) ThePairAssignments(string row)
-    {
-        var pairs = row.Split(',');
-
         Func<string, Range> ToRange = (pair) => {
             var assignments = pair.Split('-').Select(int.Parse).ToArray();
             return new Range(assignments[0], assignments[1]);
         };
 
+        var pairs = row.Split(',');
+
         return new (ToRange(pairs[0]), ToRange(pairs[1]));
     }
 
-    protected override int Part2() => 0;
+    protected override int Part2() => ChallengeDataRows
+        .Select(PairAssigmmentsToRanges)
+        .Count(PairsThatOverlap);
 
-
+    private static bool PairsThatOverlap((Range pair1, Range pair2) pairs) => 
+        pairs.pair1.Overlaps(pairs.pair2);
 }
 
 public static class RangeExtensions
 {
-    public static bool Contains(this Range value, Range otherRange) => 
-        value.Start.Value >= otherRange.Start.Value
-        && value.End.Value <= otherRange.End.Value;
+    public static bool Contains(this Range thisRange, Range otherRange) => 
+        thisRange.Start.Value >= otherRange.Start.Value
+        && thisRange.End.Value <= otherRange.End.Value;
+
+    public static bool Overlaps(this Range thisRange, Range otherRange) =>
+        thisRange.End.Value >= otherRange.Start.Value
+        && thisRange.Start.Value <= otherRange.End.Value;
 }
--- Day04/CampCleanupTests.cs
+++ Day04/CampCleanupTests.cs
@@ -5,8 +5,8 @@ public class CampCleanupTests
     [Theory]
     [InlineData(ChallengePart.Part1, InputTypes.Example, 2)]
     [InlineData(ChallengePart.Part1, InputTypes.Input, 595)]
-    // [InlineData(ChallengePart.Part2, InputTypes.Example, 70)]
-    // [InlineData(ChallengePart.Part2, InputTypes.Input, 0)]
+    [InlineData(ChallengePart.Part2, InputTypes.Example, 4)]
+    [InlineData(ChallengePart.Part2, InputTypes.Input, 952)]
     public void ChallengeShouldGiveCorrectAnswers(ChallengePart challengePart, InputTypes inputType, int expectedAnswer)
     {
         var data = ChallengeDataReader.GetDataForDay(4, inputType);
